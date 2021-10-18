9d63d707b90e60cf2385af2c5d6b82ffd8d0bcb2
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-10-17 20:01:27 -0400

    Added sad page with style sheet and javascript

--- sad/script.js
+++ sad/script.js
@@ -33,3 +33,61 @@ const removeActiveClasses = () => {
         thing.classList.remove('active');
     });
 }
+
+// const left = document.getElementById('left');
+
+// const right = document.getElementById('right');
+
+// const sad = document.getElementById('sad');
+
+// const area = document.getElementById('area');
+
+// var arrowclick = 0;
+
+// const turnright = () => {
+//     arrowclick++;
+//     if (arrowclick === 1){
+//         left.classList.remove('gone');
+//         sad.classList.remove('active-slide');
+//         area.classList.remove('inactive');
+//         area.classList.add('active-slide');
+//         console.log('cum');
+//     }
+// };
+
+// const turnleft = () => {
+//     arrowclick--;
+//     if(arrowclick === 0){
+//         left.classList.add('gone');
+//         area.classList.remove('active-slide');
+//         sad.classList.add('active-slide');
+//     }
+// }
+
+// right.addEventListener('click', turnright);
+
+// left.addEventListener('click', turnleft);
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
