71f046fc511469f7bf042ab3e5f6db4c54ab985d
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-12-03 23:49:38 -0500

    Updated most headers and updated links in footer. Updated emotions and feelings pages.Fixed issues with some links. Moved all the JS and CSS into one file. Moved html into one place.

---
+++ script.js
@@ -0,0 +1,152 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+//Sliders for emotions
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length - 1) {
+        next.style.opacity = "0"
+        next.disabled = true;
+    } else {
+        next.style.opacity = "1";
+        next.disabled = false;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    if(n === 1){
+        prev.style.opacity = "0";
+        prev.disabled = true;
+    } else {
+        prev.style.opacity = "1";
+        prev.disabled = false;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+
+//The family cube on the angry page
+var angryCube = document.getElementById('angry-family-cube');
+
+//Text under the maze 
+var angryOptions = document.querySelector('.maze-options');
+
+var angryCubeClick = 0;
+
+//Maze found on the angry page
+var angryMaze = document.getElementById('maze');
+
+//Ball inside of the maze
+var mazeball = document.getElementById('ball');
+
+var wheelclick = 0;
+
+//END OF ANGRY ELEMENTS
+
+//CONFUSED ELEMENTS
+
+//EMOTIONS ELEMENTS
+
+const angry = document.getElementById('angry-svg');
+const stressed = document.getElementById('stressed-svg');
+const lif = document.getElementById('lif-svg');
+const confused = document.getElementById('confused-svg');
+const sad = document.getElementById('sad-svg');
+
+const angrysite = () => {
+    document.location.href = "angry-index.html";
+}
+
+const stressedsite = () => {
+    document.location.href = "stressed-index.html";
+}
+
+const lifsite = () => {
+    document.location.href = "happy-index.html";
+}
+
+const confusedsite = () => {
+    document.location.href = "confused-index.html";
+}
+
+const sadsite = () => {
+    document.location.href = "sad-index.html";
+}
+
+angry.addEventListener('click', angrysite);
+stressed.addEventListener('click', stressedsite);
+lif.addEventListener('click', lifsite);
+confused.addEventListener('click', confusedsite);
+sad.addEventListener('click', sadsite);
+
+//END OF EMOTIONS ELEMENTS
+
+//HAPPY ELEMENTS 
+
+//FUNCTIONS FOR VARIOUS ELEMENTS 
+
+var spinTheAngryCube = () => {
+    angryCubeClick++;
+    if(angryCubeClick === 1){
+        angryCube.classList.remove('angry-cube-1');
+        angryCube.classList.add('angry-cube-2');
+        angryOptions.innerHTML = "<p>Friends</p>";
+    }else if (angryCubeClick === 2){
+        angryCube.classList.remove('angry-cube-2');
+        angryCube.classList.add('angry-cube-3');
+        angryOptions.innerHTML = "<p>School</p>";
+    }else {
+        angryCube.classList.remove('angry-cube-3');
+        angryCube.classList.add('angry-cube-1');
+        angryOptions.innerHTML = "<p>Family</p>";
+        angryCubeClick = 0;
+    }
+}
+
+angryCube.addEventListener('click', spinTheAngryCube);
+
+
+var turnTheMaze = () => {
+    wheelclick++;
+
+    if(wheelclick === 1){
+        mazeball.classList.remove('maze-1');
+        mazeball.classList.add('maze-2');
+    } else if (wheelclick === 2){
+        mazeball.classList.remove('maze-2');
+        mazeball.classList.add('maze-3');
+    } else if (wheelclick === 3){
+        mazeball.classList.remove('maze-3');
+        mazeball.classList.add('maze-4');
+    } else if (wheelclick === 4){
+        mazeball.classList.remove('maze-4');
+        mazeball.classList.add('maze-5');
+    } else {
+        returnmaze();
+    }
+}
+
+const returnmaze = () => {
+    wheelclick = 0;
+    mazeball.classList.remove('maze-5');
+    mazeball.classList.add('maze-1');
+}
+
+maze.addEventListener('click', turnTheMaze);
+
