727645eb82a25ae682f86e1439462dae3b04ae9f
Henry_DelGlitch <CodingWHenry@gmail.com>
2022-12-12 16:53:14 -0500

    Updated Happy styling, added Spin animation back, and updated Sad styling.

--- js/script.js
+++ js/script.js
@@ -6,18 +6,18 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-var i;
-var slides = document.getElementsByClassName("mySlides");
-    if(n > slides.length - 1) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length - 1) {
         next.classList.add("isDisabled");
     } else {
         next.classList.remove("isDisabled");
@@ -25,7 +25,7 @@ var slides = document.getElementsByClassName("mySlides");
     if (n < 1) {
         slideIndex = slides.length;
     }
-    if(n === 1){
+    if (n === 1) {
         prev.classList.add("isDisabled");
     } else {
         prev.classList.remove("isDisabled");
@@ -33,94 +33,19 @@ var slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex - 1].style.display = "block";
 }
 
-const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    console.log("fuck you");
 }
-
-hamburger.addEventListener("click", mobileMenu);
 
 const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
-
-//The family cube on the angry page
-var angryCube = document.getElementById('angry-family-cube');
-
-//Text under the maze 
-var angryOptions = document.querySelector('.maze-options');
-
-var angryCubeClick = 0;
-
-//Maze found on the angry page
-var angryMaze = document.getElementById('maze');
-
-//Ball inside of the maze
-var mazeball = document.getElementById('ball');
-
-var wheelclick = 0;
-
-//END OF ANGRY ELEMENTS
-
-//FUNCTIONS FOR VARIOUS ELEMENTS 
-
-var spinTheAngryCube = () => {
-    angryCubeClick++;
-    if(angryCubeClick === 1){
-        angryCube.classList.remove('angry-cube-1');
-        angryCube.classList.add('angry-cube-2');
-        angryOptions.innerHTML = "<p>Friends</p>";
-    }else if (angryCubeClick === 2){
-        angryCube.classList.remove('angry-cube-2');
-        angryCube.classList.add('angry-cube-3');
-        angryOptions.innerHTML = "<p>School</p>";
-    }else {
-        angryCube.classList.remove('angry-cube-3');
-        angryCube.classList.add('angry-cube-1');
-        angryOptions.innerHTML = "<p>Family</p>";
-        angryCubeClick = 0;
-    }
-}
-
-angryCube.addEventListener('click', spinTheAngryCube);
-
-var turnTheMaze = () => {
-    wheelclick++;
-
-    if(wheelclick === 1){
-        mazeball.classList.remove('maze-1');
-        mazeball.classList.add('maze-2');
-    } else if (wheelclick === 2){
-        mazeball.classList.remove('maze-2');
-        mazeball.classList.add('maze-3');
-    } else if (wheelclick === 3){
-        mazeball.classList.remove('maze-3');
-        mazeball.classList.add('maze-4');
-    } else if (wheelclick === 4){
-        mazeball.classList.remove('maze-4');
-        mazeball.classList.add('maze-5');
-    } else {
-        returnmaze();
-    }
-}
-
-const returnmaze = () => {
-    wheelclick = 0;
-    mazeball.classList.remove('maze-5');
-    mazeball.classList.add('maze-1');
-}
-
-maze.addEventListener('click', turnTheMaze);
-
